5bcdca1304f41b25298e96d1add87760c0853f9c
Augusto Silva <augusto.a.silva@hotmail.com>
2025-07-19 15:35:51 -0300

    adiciona novas funções, métodos e rotas para adicionar, atualizar e excluir um todo da base

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,8 +11,15 @@ func main() {
 	service := todo.NewService()
 	handler := todo.NewHandler(service)
 
-	http.HandleFunc("/todos", handler.ListTodos)
-	// Aqui depois vamos adicionar nossar outras rotas
+	// agora que temos mais rotas, e as rotas acabam se duplicando, para identificar qual rota combina com qual verbo http
+	// e assim poder escolher o handler correto, é desta forma que fica usando o pacote padrão http do Go
+	http.HandleFunc("GET /todos", handler.ListTodos)
+	http.HandleFunc("POST /todos", handler.PostTodo)
+
+	// e no caso de querermos passar um parametro ao final da url, que geralmente fazemos passando algum id, usamos um
+	// wildcard na url, que é como chamamos o {} aqui. para este caso estamos usando {id}
+	http.HandleFunc("PUT /todos/{id}", handler.PutTodo)
+	http.HandleFunc("DELETE /todos/{id}", handler.DeleteTodo)
 
 	fmt.Println("Servidor iniciado em local host na porta 8080")
 	_ = http.ListenAndServe(":8080", nil)
--- internal/todo/service.go
+++ internal/todo/service.go
@@ -5,7 +5,8 @@ import (
 )
 
 // A service aqui que criamos vai conter a logica, ela é um objet (struct) pois mais para frente iremos injetar a dependencia
-// nela, calma que vai fazer sentido em outro artigo
+// nela, calma que vai fazer sentido em outro artigo. Também adiciono aqui neste artigo que por hora não temos validações
+// de regra de negócio, ainda, mas em outro artigo já vamos adicionar algumas.
 type Service struct {
 }
 
@@ -16,4 +17,19 @@ func NewService() Service {
 // Dessa forma aque fizemos, basta que o método da service chame a função da model para que list todos os todos.
 func (s Service) ListTodos(ctx context.Context) []Todo {
 	return GetAllTodos(ctx)
+}
+
+// igualmente para inserir um novo todos
+func (s Service) InsertTodo(ctx context.Context, todo Todo) error {
+	return AddTodo(ctx, todo)
+}
+
+// igualmente para atualizar um todos
+func (s Service) UpdateTodo(ctx context.Context, todo Todo) error {
+	return UpdateTodo(ctx, todo)
+}
+
+// e para deletar um todos
+func (s Service) DeleteTodo(ctx context.Context, id int) error {
+	return DeleteTodo(ctx, id)
 }
